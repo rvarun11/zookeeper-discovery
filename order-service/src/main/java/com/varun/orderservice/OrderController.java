d3d57b23ad2563068e7b42d2c623698682b34a7d
rvarun011 <varunrajput11@zoho.com>
2021-04-16 00:49:08 +0530

    had to use @Controller for redirect prefix to work

--- order-service/src/main/java/com/varun/orderservice/OrderController.java
+++ order-service/src/main/java/com/varun/orderservice/OrderController.java
@@ -3,10 +3,11 @@ package com.varun.orderservice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+// can't use RestController because redirect prefix won't work.
+@Controller
 @RequestMapping("order")
 public class OrderController {
     private static final Logger log = LoggerFactory.getLogger(OrderController.class);
@@ -14,17 +15,19 @@ public class OrderController {
     @Autowired
     ServiceManager serviceManager;
 
-    //@PostMapping(value = "place", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @GetMapping(value ="place")
-    public RedirectView placeOrder() throws Exception {
+    @RequestMapping(value = "place")
+    @ResponseBody
+    public String placeOrder() {
         log.info("confirmOrder was hit");
-        //Retrieving the IP & Port of Payment Service
-        ServiceData paymentService = new ServiceData(serviceManager.getService("PaymentService"));
-        String url = "http://" + paymentService.getIp() + ":" + paymentService.getPort() + "/payment/create";
 
-        //redirect prefix isn't working so have to use RedirectView!
-        //return "redirect:" + url;
-        return new RedirectView(url);
+        try {
+            ServiceData paymentService = new ServiceData(serviceManager.getService("PaymentService"));
+            return "redirect:http://" + paymentService.getIp() + ":" + paymentService.getPort() + "/payment/create";
+        }
+        catch (Exception e) {
+            log.info("PaymentService is down");
+            return "PaymentService is currently down! Please try again later.";
+        }
     }
 
 }
